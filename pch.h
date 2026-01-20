af49360644f98b29c38dff9edd9dcb36b578a966
aozhuoyi76-sketch <aozhuoyi76@gmail.com>
2026-01-21 03:46:29 +0800

    Add precompiled header file pch.h

---
+++ pch.h
@@ -0,0 +1,14 @@
+#ifndef PCH_H
+#define PCH_H
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <afxwin.h>
+#include <afxext.h>
+#include <afxdtl.h>
+#include <afxisapi.h>
+#include <afxdisp.h>
+#include <cmath>
+#include <vector>
+
+#endif
